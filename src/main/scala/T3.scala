03df32582c221d33e06de140023e851a9455e165
zhaobing <zhaobing2011@163.com>
2019-07-19 23:16:51 +0800

    增加一行输出

--- src/main/scala/T3.scala
+++ src/main/scala/T3.scala
@@ -2,7 +2,8 @@ import stdlib.StdOut
 
 object T3 {
     def main(args: Array[String]): Unit = {
-        println("测试", "scala")
+        println("测试", "Scala")
+        println("测试", "Scala")
         StdOut.print("Test-StdOut-Scala")
     }
 }
